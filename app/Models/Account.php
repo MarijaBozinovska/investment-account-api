2511b8a33a2674ea4a8ff624ffee356a41ad3143
BranislavTomik <45269540+BranislavTomik@users.noreply.github.com>
2026-09-04 16:07:11 +0200

    Add transaction database structure

--- app/Models/Account.php
+++ app/Models/Account.php
@@ -4,11 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Account extends Model
 {
     public function client(): BelongsTo
     {
         return $this->belongsTo(Client::class);
+    }
+
+    public function transactions(): HasMany
+    {
+        return $this->hasMany(Transaction::class);
     }
 }
